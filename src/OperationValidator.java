21f72e6b4ac7ca5e8a79b17e0d3dbe54e3699db6
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-02 15:00:32 +0100

    fhshf

--- src/OperationValidator.java
+++ src/OperationValidator.java
@@ -1,5 +1,11 @@
+import java.util.regex.Pattern;
+
 public class OperationValidator {
-    public static void validate(String działanie) {
-        throw new NoSuchMethodError();
+    public static void validate(String działanie){
+        //[+-/*]
+        if (działanie.trim().length() >1 &&
+                !działanie.trim().matches("[+-/*]/g"))
+            throw new UnsupportedOperationException(
+                    "To nie jest znak działania matematycznego!");
     }
 }
