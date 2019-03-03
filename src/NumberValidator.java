aa1f762cd9546620a5d68b90f8e9f4af70bdfbf3
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-03 09:50:52 +0100

    Podane parametry w wywołanej metodzie giveAnswer.

--- src/NumberValidator.java
+++ src/NumberValidator.java
@@ -1,9 +1,14 @@
 public class NumberValidator {
-    public static void validate(String liczba1) {
-        try {
-            Double.parseDouble(liczba1);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Wpisałeś nieprawidłową wartość.");
-        }
+    public static void validate(String liczba1) throws IllegalAccessException {
+
+        if (!liczba1.trim().matches("\\d+")) //plus powodyuje, ze nie ma przew miedzy znakami
+            throw new IllegalAccessException("To nie jest liczba.");
+
+
+//        try {
+//            Double.parseDouble(liczba1);
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Wpisałeś nieprawidłową wartość.");
+//        }
     }
 }
