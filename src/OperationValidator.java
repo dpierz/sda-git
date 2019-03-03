64b79af0e9e6b7182c21acbbdabf5724cf1f955f
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-03 01:35:01 +0100

    Nic istotnego.

--- src/OperationValidator.java
+++ src/OperationValidator.java
@@ -1,17 +1,11 @@
 import java.util.regex.Pattern;
 
 public class OperationValidator {
-    public static void validate(String działanie){
+    public static void validate(String działanie) {
         //[+-/*]
-        if (działanie.trim().length() >1 &&
+        if (działanie.trim().length() > 1 &&
                 !działanie.trim().matches("[+-/*]/g"))
             throw new UnsupportedOperationException(
                     "To nie jest znak działania matematycznego!");
-
-        //abc
-        //jjkk
-        //kakkskakdaldkald
     }
 }
-//kome
-//comment
