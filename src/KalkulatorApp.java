d4280bddc7d7c519b2515610df9116fe976b92d2
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-03 10:01:52 +0100

    Commit all.

--- src/KalkulatorApp.java
+++ src/KalkulatorApp.java
@@ -1,14 +1,24 @@
 public class KalkulatorApp {
     public static void main(String[] args) {
         String działanie = InputManager.getOperation();
-        OperationValidator.validate(działanie); //todo obsługa wyjatku
+        try {
+            OperationValidator.validate(działanie); //todo obsługa wyjatku
+        }catch (IllegalAccessException e){
+            e.printStackTrace();
+            działanie = InputManager.getOperation();
+        }
+
 
         String liczba1 = InputManager.getNumber(); //todo obsługa wyjatku
-        NumberValidator.validate(liczba1);
+        //NumberValidator.validate(liczba1);
 
         String liczba2 = InputManager.getNumber(); //todo obsługa wyjatku
-        NumberValidator.validate(liczba2);
+        //umberValidator.validate(liczba2);
 
-        System.out.println(Operations.giveAnswer("+", 5, 5)); //todo obsługa wyjatku... czy napewno?
+        try {
+            System.out.println(Operations.giveAnswer(działanie, liczba1, liczba2)); //todo obsługa wyjatku... czy napewno?
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/OperationValidator.java
+++ src/OperationValidator.java
@@ -1,11 +1,16 @@
 import java.util.regex.Pattern;
 
 public class OperationValidator {
-    public static void validate(String działanie) {
+    public static void validate(String działanie) throws IllegalAccessException {
+        //sprawdzamy czy długosc stringa jest 1 lub mniejsza
+        //przewidujemy tylko pojetynczy znak!
         //[+-/*]
-        if (działanie.trim().length() > 1 &&
+        if (działanie.trim().length() >1 &&
+                //sprawdzamy  czy znak jest znakiem operacji
                 !działanie.trim().matches("[+-/*]/g"))
+            //jeżeli warunek jest spełniony rzucamy wyjątkiem
             throw new UnsupportedOperationException(
                     "To nie jest znak działania matematycznego!");
+
     }
 }
