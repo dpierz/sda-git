e3c08a07fefa6a0f8603a4c8c5f5a21d49310725
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-03 01:37:35 +0100

    Próba rozwiązania zadania (nieudana).

--- src/Operations.java
+++ src/Operations.java
@@ -1,5 +1,20 @@
 public class Operations {
-    public static int giveAnswer() {
-        throw new NoSuchMethodError();
+    public static int giveAnswer(String działanie, Integer liczba1, Integer liczba2) {
+
+        switch (działanie) {
+            case "+":
+                return (liczba1 + liczba2);
+            case "-":
+                return (liczba1 - liczba2);
+            case "*":
+                return (liczba1 * liczba2);
+            case "/":
+                if (liczba2 == 0) {
+                    System.out.println("Nie dziel cholero przez zero.");
+                } else {
+                    return (liczba1 / liczba2);
+                }
+        }
+        return 000000;
     }
 }
