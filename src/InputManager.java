771df20094c19377768a0c7df5a246fc45456eaa
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-03 09:37:09 +0100

    Podane parametry w wywołanej metodzie giveAnswer.

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,9 +1,22 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class InputManager {
+    private static Scanner scanner = new Scanner(System.in); //dlatego statyczny,bo metoda get Operation jest statyczna
+
     public static String getOperation() {
-        throw new NoSuchMethodError();
+        System.out.println("Podaj znak operacji matematycznej," + " którą chcesz wykonać +,-,*,/:");
+       return scanner.next();
+
+//        return JOptionPane.showInputDialog("Podaj znak operacji matematycznej, którą chcesz wykonać +,-,*,/:");//wyświetli się okienko, w które możemy wpisać znak operacji
+//        new StringBuilder("").append("").toString();
     }
 
     public static String getNumber() {
-        throw new NoSuchMethodError();
+        System.out.println("Podaj liczbę dla której chcesz wykonać operację:");
+        return scanner.next();
     }
-}
+
+//    public static void main(String[] args) {
+//        System.out.println(getOperation());
+    }
