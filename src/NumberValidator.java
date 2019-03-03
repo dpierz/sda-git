dbf5d87c191416c8176c5b7d7f8cf1dc69bb0d4d
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-03 09:52:33 +0100

    Zmiana komentarza.

--- src/NumberValidator.java
+++ src/NumberValidator.java
@@ -1,6 +1,6 @@
 public class NumberValidator {
     public static void validate(String liczba1) throws IllegalAccessException {
-//plus jka sie okazuje jest istotny
+//plus jka sie okazuje jest istotny 
         if (!liczba1.trim().matches("\\d+")) //plus powodyuje, ze nie ma przew miedzy znakami
             throw new IllegalAccessException("To nie jest liczba.");
 
